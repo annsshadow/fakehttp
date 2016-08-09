62f524cc23821fc68ff18c76861bf06fd0c59719
shadowwen-PC0\Administrator <cravenboy@163.com>
2016-08-09 14:23:51 +0800

    second try of svr

--- svr.c
+++ svr.c
@@ -6,85 +6,115 @@
     > Function: simple test svr 
 ************************************************************************/
 
-#include <sys/time.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
+#include <string.h> // bzero, strerror  
+#include <errno.h> // errno  
+#include <stdio.h>  // fprintf, fflush  
+#include <stdlib.h>  // exit  
+#include <sys/socket.h> // socket, bind, listen, accept  
+#include <netinet/in.h> // struct socketaddr_t, struct in_addr, htonl, htons  
+#include <unistd.h> // fork  
+#include <signal.h> // signal  
 #include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#include <sys/wait.h>   // waitpid  
+#include <pthread.h>    // pthread_create
+#include <time.h>   //for time
 
-#define PORT 12321
-#define BACKLOG 32
-#define MAXRECVLEN 1024
+#define MAX_BUF_SIZE 512
+#define MAX_LISTEN_QUEUE_LENGTH 512
 
-int main(int argc, char *argv[])
-{
-    char buf[MAXRECVLEN];
-    int listenfd, connectfd;  /* socket descriptors */
-    struct sockaddr_in server; /* server's address information */
-    struct sockaddr_in client; /* client's address information */
-    socklen_t addrlen;
-    /* Create TCP socket */
-    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-    {
-        /* handle exception */
-        perror("socket() error. Failed to initiate a socket");
-        exit(1);
+int Socket(int domain, int type, int protocol) {
+    int sock = socket(domain, type, protocol);
+    if (sock < 0) {
+        SYSERR("fail to init socket fd");
     }
- 
-    /* set socket option */
-    int opt = SO_REUSEADDR;
-    setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    return sock;
+}
 
-    bzero(&server, sizeof(server));
-
-    server.sin_family = AF_INET;
-    server.sin_port = htons(PORT);
-    server.sin_addr.s_addr = htonl(INADDR_ANY);
-    if(bind(listenfd, (struct sockaddr *)&server, sizeof(server)) == -1)
-    {
-        /* handle exception */
-        perror("Bind() error.");
-        exit(1);
+void Listen(int sock) {
+    int ret = listen(sock, MAX_LISTEN_QUEUE_LENGTH);
+    if (ret != 0) {
+        SYSERR("fail to listen on socket %d", sock);
     }
-    
-    if(listen(listenfd, BACKLOG) == -1)
-    {
-        perror("listen() error. \n");
-        exit(1);
-    }
+}
 
-    addrlen = sizeof(client);
-    while(1){
-        if((connectfd=accept(listenfd,(struct sockaddr *)&client, &addrlen))==-1)
-          {
-            perror("accept() error. \n");
-            exit(1);
-          }
-
-        struct timeval tv;
-        gettimeofday(&tv, NULL);
-          printf("You got a connection from client's ip %s, port %d at time %ld.%ld\n",inet_ntoa(client.sin_addr),htons(client.sin_port), tv.tv_sec,tv.tv_usec);
-        
-        int iret=-1;
-        while(1)
-        {
-            iret = recv(connectfd, buf, MAXRECVLEN, 0);
-            if(iret>0)
-            {
-                printf("%s\n", buf);
-            }else
-            {
-                close(connectfd);
-                break;
-            }
-            /* print client's ip and port */
-            send(connectfd, buf, iret, 0); /* send to the client welcome message */
-        }
+void Bind(int sock, const struct sockaddr_in* servaddr) {
+    // use struct sockaddr for compatibility of history reason
+    int ret = bind(sock, (const struct sockaddr*)servaddr, sizeof(struct sockaddr));
+    if (ret != 0) {
+        SYSERR("fail to bind socket %d", sock);
     }
-    close(listenfd); /* close listenfd */
+}
+
+int Accept(int sock, struct sockaddr_in* cliaddr, socklen_t* cli_len) {
+    int ret = accept(sock, (struct sockaddr*)cliaddr, cli_len);
+    NOTICE("accpet and give socket %d", ret);
+    if (ret < 0) {
+        SYSERR("fail to accept");
+    }
+    return ret;
+}
+
+void single_echo(int conn_sock) {
+    char buf[MAX_BUF_SIZE];
+    time_t t;
+    struct tm *gmt;
+    char *gmtp=NULL;
+    tzset();
+    t=time(NULL);
+    gmt=gmtime(&t);
+    gmtp=asctime(gmt);
+    strcpy(buf,gmtp);
+    send(conn_sock,buf,strlen(buf),MSG_DONTWAIT);
+}
+
+pthread_t PthreadCreate(int conn_sock, void* run(void*)) {
+    pthread_t tid;
+    int sock = conn_sock;
+    int ret = pthread_create(&tid, NULL, run, (void*)&sock);
+    if (ret) {
+        POSIXERR(ret, "fail to create thread");
+    }
+    return tid;
+}
+
+void* thread_run(void* arg) {
+    int sock = *(int*)arg;
+    single_echo(sock);
+    return NULL;
+}
+
+int main(int argc, char** argv) {
+
+    // init port
+    int port = 12321;
+
+    // init sock fd
+    int sock = Socket(PF_INET, SOCK_STREAM, 0);
+
+    // init server sock address
+    struct sockaddr_in  servaddr;
+    bzero(&servaddr, sizeof(servaddr));
+
+    // populate protocol fields
+    servaddr.sin_family        = AF_INET;
+    servaddr.sin_addr.s_addr   = htonl(INADDR_ANY);
+    servaddr.sin_port          = htons(port);
+
+    // bind sock fd and sock address
+    Bind(sock, &servaddr);
+
+    // start to listen
+    Listen(sock);
+
+    // init client address
+    struct sockaddr_in cliaddr;
+    socklen_t cli_len = sizeof(cliaddr);
+
+    while (1) {
+        // accpet connection request
+        int conn_sock = Accept(sock, &cliaddr, &cli_len);
+        PthreadCreate(conn_sock, thread_run);
+    }
     return 0;
+
 }
